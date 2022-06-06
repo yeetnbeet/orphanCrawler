a4447ae896836503970f7b30fa1f974038bb8c6c
Ryan Littlefield <segalcsam@gmail.com>
2022-06-06 14:34:08 -0600

    still staging

---
+++ README.txt
@@ -0,0 +1,5 @@
+Welcome to the Contender Orphan Page utility
+
+dependancies-
+requests==2.22.0 beautifulsoup4==4.8.1 
+Matlibplot==
---
+++ orph.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
+html_text = requests.get(vgm_url).text
+soup = BeautifulSoup(html_text, 'html.parser')
