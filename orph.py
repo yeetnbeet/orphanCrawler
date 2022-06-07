43fe0197cc630bc183634cbbc42dac2c3617f067
Ryan Littlefield <segalcsam@gmail.com>
2022-06-06 18:39:19 -0600

    working

--- orph.py
+++ orph.py
@@ -1,7 +1,54 @@
+from numpy import array
 import requests
+import csv
 from bs4 import BeautifulSoup
 
+# 1. import csv of target urls ~create new csv maybe~
+# 2. while loop through targets and find id="AddToCart" and disabled="disabled"
+# add to 3D array [[URL,NAME,STATUS],[URL,NAME,STATUS],...]
+# 3. Export to new csv or plot the data...
+count = 0
+empty = []
+instockAlert = []
+noStock = []
 
-vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
-html_text = requests.get(vgm_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
+with open('DATA.csv') as csvfile:
+    array = csv.reader(csvfile)
+    loopableArray = list(array)
+
+for line in loopableArray:
+    html_text = requests.get(line[0]).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    print(count)
+    if soup.find_all("button",{'disabled':'disabled'}) == empty:        
+        instockAlert.append(line[0],line[1])
+    else:        
+        noStock.append(line[0],line[1])
+    
+    count = count+1
+
+print(instockAlert)
+print("out stock")
+print(noStock)
+
+#html_text = requests.get('https://contenderbicycles.com/collections/apparel/products/contender-pride-t-shirt').text
+#soup = BeautifulSoup(html_text, 'html.parser')
+#if soup.find_all("button",{'disabled':'disabled'})== empty:
+    #print("instock")
+#else:
+    #print("out")
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+    
